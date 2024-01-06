9f321839b46b9fde8c42fac06103b18679eced09
Tarik Besic <tarikdalmatia@gmail.com>
2024-01-06 21:58:38 +0100

    Revert "improve loading experience by adding partial prerendering feature"
    This reverts commit ef6ec0db7117ace653245b04319a32836a98ff96.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+import React from "react";
+import SideNav from "../ui/dashboard/sidenav";
+import DashboardHeader from "../ui/dashboard/header/dashboard-header";
+import Footer from "../ui/dashboard/footer";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-auto min-h-screen flex-row">
+      <SideNav />
+      <div className="flex flex-1 flex-col bg-[#ff0000b8]">
+        <DashboardHeader />
+        <div className="flex w-full flex-1 flex-col bg-[#f2f2f2]">
+          <div className="w-full flex-1 px-10">{children}</div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
